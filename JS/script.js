5012b773fdba47b6ed5783765e73b6c917e9daed
matthew7621 <matthewgreat92@gmail.com>
2022-06-14 13:38:37 +0300

    Конец 12го урока

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
-alert("Закончил смотреть 10ый урок");
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцениваете его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцениваете его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB); 
